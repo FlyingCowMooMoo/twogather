9a0c4ba57efc0698d6c4820b78a09c7e6e4d0d9e
Peter Papastamatis <Peter Papastamatis>
2016-03-29 15:50:07 +1100

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='twogather',
+    version='',
+    packages=['app'],
+    url='',
+    license='',
+    author='petez',
+    author_email='',
+    description=''
+)
